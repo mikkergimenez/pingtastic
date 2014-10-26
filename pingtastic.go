c5aaddff2df2aa4d19541839d74c3208a1fdf8ae
Mikker Gimenez-Peterson <mikkergimenez@gmail.com>
2014-10-26 11:06:06 -0700

    Bugfixes, completing Average Calculations

--- pingtastic.go
+++ pingtastic.go
@@ -1,4 +1,18 @@
-// A Program that reads data from the alexa top 1000 sites and pings them periodically.
+/*
+ * A Program that reads data from the alexa top 1000 sites and pings them periodically.
+ *
+ * This program can be run either as a server:
+ *
+ * ./pingtastic server
+ *
+ * Periodically however, and prior to first run, you will want to download 
+ * the Alexa Top 1000 and import the data to the databse.
+ * 
+ * This will be done by running:
+ *
+ * ./pingtastic getAlexa
+ * 
+ */ 
 package main
 
 import (
@@ -6,41 +20,48 @@ import (
 	"fmt"
 	//"github.com/tatsushid/go-fastping"
 	"database/sql"
-  _ "github.com/go-sql-driver/mysql"
 	"github.com/aeden/traceroute"
-	"strconv"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 	"net"
+	"os"
+	"encoding/json"
+	"strconv"
 	"strings"
 	"time"
-	"os"
-	"log"
 	//"syscall"
-	//"os/signal"	
+	//"os/signal"
 )
 
+type Configuration struct {
+	DBUser		string
+	DBPass		string
+	DBDatabase	string
+	DBHost		string
+	DBPort		string
+	AlexaURL	string
+}
+
 type MysqlServer struct {
-    url string
-    user string
-    password string
-    database string
-}
-
-type Website struct {
-    Url string
-    Name string
-    PingValue int
-}
-
-type Layout struct {
-	x_range, y_range int
+	url      string
+	user     string
+	password string
+	database string
 }
 
 type Ping struct {
-	urlid int
-	rtt int
-	url string
+	urlid  string
+	rtt    int
+	url    string
 	ipaddr string
 }
+
+type Website struct {
+	Url       string
+	Name      string
+	PingValue int
+}
+
 
 func doPing(pingTime Ping) int {
 
@@ -54,53 +75,137 @@ func doPing(pingTime Ping) int {
 	// fmt.Println(pingTime.ipaddr, " ", elapsed, " ", elapsed.Seconds(), " ", int(elapsed))
 
 	return_time := int(elapsed.Seconds() * 1000)
-	
+
 	return return_time
 }
 
+func get_day_average(db *sql.DB, ping Ping, hour int) {
+	var day_query string 
+	var update_day_query string
+
+	day_query = "SELECT latency FROM pings WHERE time_of >= now() - INTERVAL 1 DAY and url_id="+ping.urlid+";"
+    rows, err := db.Query(day_query)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer rows.Close()
+
+    total := 0
+    count := 0
+    for rows.Next() {
+        var latency int
+        if err := rows.Scan(&latency); err != nil {
+            log.Fatal(err)
+        }
+        total += latency
+        count += 1
+    }
+
+    var day_average int
+    if count > 0 {
+    	day_average = total / count
+    } else {
+    	day_average = 0
+    }
+
+    update_day_query = "INSERT INTO latency_averages (url_id, past_24_hours) VALUES ("+ping.urlid+", "+strconv.Itoa(day_average)+") ON DUPLICATE KEY UPDATE past_24_hours="+strconv.Itoa(day_average)
+    
+    _, err = db.Exec(update_day_query)
+    if err != nil {
+        fmt.Printf("Error (get_day_average) : ", err)
+    }
+
+	// This function calculates the average latency for the past 24 hours
+}
+
+func get_hour_average(db *sql.DB, ping Ping, hour int) {
+	// This function calculates the average latency for this hour of the day
+	var hour_query string
+	var update_hour_query string
+	hour_query = "SELECT latency FROM pings WHERE hour_of="+strconv.Itoa(hour)+" and url_id="+ping.urlid+";"
+    rows, err := db.Query(hour_query)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer rows.Close()
+
+    total := 0
+    count := 0
+    for rows.Next() {
+        var latency int
+        if err := rows.Scan(&latency); err != nil {
+            log.Fatal(err)
+        }
+        total += latency
+        count += 1
+    }
+
+    var hour_average int
+    if count > 0 {
+    	hour_average = total / count
+    } else {
+    	hour_average = 0
+    }
+
+    update_hour_query = "INSERT INTO latency_averages (url_id, hour_"+strconv.Itoa(hour)+") VALUES ("+ping.urlid+", "+strconv.Itoa(hour_average)+") ON DUPLICATE KEY UPDATE hour_"+strconv.Itoa(hour)+"="+strconv.Itoa(hour_average)
+    _, err = db.Exec(update_hour_query)
+    
+    if err != nil {
+        fmt.Printf("Error (get_hour_average) : ", err)
+    }
+
+
+}
+
 func writeToDB(db *sql.DB, ping Ping) {
-    var dbquery string
-    var urlquery string
-    var url_update_query string
- 	var urlid int
+	var dbquery string
+	var urlquery string
+	var url_update_query string
+	var urlid int
+	var current_hour int
 
-    urlquery = "SELECT id from urls where ip ='"+ping.ipaddr+"'"
-    err := db.QueryRow(urlquery).Scan(&urlid)
+	urlquery = "SELECT id from urls where ip ='" + ping.ipaddr + "'"
+	err := db.QueryRow(urlquery).Scan(&urlid)
 
-    const layout = "2014-12-30 03:04:58"
-    dbquery = "INSERT INTO pings (url_id, latency, time_of) VALUES ('"+strconv.Itoa(urlid)+"', '"+strconv.Itoa(ping.rtt)+"', NOW())"
+	const layout = "2014-12-30 03:04:58"
+
+	current_hour = time.Now().Hour()
+	dbquery = "INSERT INTO pings (url_id, latency, time_of, hour_of) VALUES ('" + strconv.Itoa(urlid) + "', '" + strconv.Itoa(ping.rtt) + "', NOW(), '"+strconv.Itoa(current_hour)+"')"
 	
-    _, err = db.Exec(dbquery)
-    if err != nil {
-        log.Fatal(err)
-    }
+	_, err = db.Exec(dbquery)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	url_update_query = "UPDATE urls SET latency='"+strconv.Itoa(ping.rtt)+"' WHERE id='"+strconv.Itoa(urlid)+"'"
+	url_update_query = "UPDATE urls SET latency='" + strconv.Itoa(ping.rtt) + "' WHERE id='" + strconv.Itoa(urlid) + "'"
 
-    _, err = db.Exec(url_update_query)
-    if err != nil {
-        log.Fatal(err)
-    }
+	_, err = db.Exec(url_update_query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	get_hour_average(db, ping, current_hour)
+	get_day_average(db, ping, current_hour)
+
 }
 
 func printHop(db *sql.DB, hop traceroute.TracerouteHop, ping Ping) {
-    var dbquery string
-    var urlquery string
- 	var urlid int
+	var dbquery string
+	var urlquery string
+	var urlid int
 
-	urlquery = "SELECT id from urls where ip ='"+ping.ipaddr+"'"
-    err := db.QueryRow(urlquery).Scan(&urlid)
+	urlquery = "SELECT id from urls where ip ='" + ping.ipaddr + "'"
+	err := db.QueryRow(urlquery).Scan(&urlid)
 
 	addr := fmt.Sprintf("%v.%v.%v.%v", hop.Address[0], hop.Address[1], hop.Address[2], hop.Address[3])
-	
 	if hop.TTL < 31 {
-    	dbquery = "INSERT INTO traceroutes (url_id, hop"+strconv.Itoa(hop.TTL)+", name"+strconv.Itoa(hop.TTL)+", updated ) VALUES ('"+strconv.Itoa(urlid)+"', '"+addr+"', '"+hop.Host+"', NOW()) ON DUPLICATE KEY UPDATE hop"+strconv.Itoa(hop.TTL)+"='"+addr+"', updated=NOW(), name"+strconv.Itoa(hop.TTL)+"='"+hop.Host+"'"
-    	_, err = db.Exec(dbquery)
-    	if err != nil {
-	        log.Fatal(err)
-    	}
-    }
-    swapSort(urlid, db)
+		dbquery = "INSERT INTO traceroutes (url_id, hop" + strconv.Itoa(hop.TTL) + ", name" + strconv.Itoa(hop.TTL) + ", updated ) VALUES ('" + strconv.Itoa(urlid) + "', '" + addr + "', '" + hop.Host + "', NOW()) ON DUPLICATE KEY UPDATE hop" + strconv.Itoa(hop.TTL) + "='" + addr + "', updated=NOW(), name" + strconv.Itoa(hop.TTL) + "='" + hop.Host + "'"
+		_, err = db.Exec(dbquery)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	swapSort(urlid, db)
 }
 
 func address(address [4]byte) string {
@@ -109,15 +214,15 @@ func address(address [4]byte) string {
 
 func doTraceroute(db *sql.DB, ping Ping) {
 	/*
-	var m = flag.Int("m", traceroute.DEFAULT_MAX_HOPS, `Set the max time-to-live (max number of hops) used in outgoing probe packets (default is 64)`)
-	var q = flag.Int("q", 1, `Set the number of probes per "ttl" to nqueries (default is one probe).`)
+		var m = flag.Int("m", traceroute.DEFAULT_MAX_HOPS, `Set the max time-to-live (max number of hops) used in outgoing probe packets (default is 64)`)
+		var q = flag.Int("q", 1, `Set the number of probes per "ttl" to nqueries (default is one probe).`)
 
-	flag.Parse()
-	host := flag.Arg(0)
-	options := traceroute.TracerouteOptions{}
+		flag.Parse()
+		host := flag.Arg(0)
+		options := traceroute.TracerouteOptions{}
 
-	options.SetRetries(*q - 1)
-	options.SetMaxHops(*m + 1)
+		options.SetRetries(*q - 1)
+		options.SetMaxHops(*m + 1)
 	*/
 	options := traceroute.TracerouteOptions{}
 	options.SetRetries(0)
@@ -141,16 +246,16 @@ func doTraceroute(db *sql.DB, ping Ping) {
 
 	_, err := traceroute.Traceroute(ipAddr, &options, c)
 	if err != nil {
-		fmt.Printf("Error: ", err)
+		fmt.Printf("Error (doTraceroute) : ", err)
 	}
 }
 
 func getDBCon(db MysqlServer) *sql.DB {
-    dbcon, err := sql.Open("mysql", db.user+":"+db.password+"@"+db.url+"/"+db.database)
+	dbcon, err := sql.Open("mysql", db.user+":"+db.password+"@"+db.url+"/"+db.database)
 	if err != nil {
-        log.Fatal(err)
-    }
-    return dbcon
+		log.Fatal(err)
+	}
+	return dbcon
 }
 
 func calculatePath(db *sql.DB, ping Ping) {
@@ -163,8 +268,8 @@ func calculatePath(db *sql.DB, ping Ping) {
 	for i, _ := range container {
 		names[i] = &container[i]
 	}
-	
-	urlquery := "SELECT name1, name2, name3, name4, name5, name6, name7, name8, name9, name10, name11, name12, name13, name14, name15, name16, name17, name18, name19, name20, name21, name22, name23, name24, name25, name26, name27, name28, name29, name30 FROM traceroutes WHERE url_id="+strconv.Itoa(ping.urlid)
+
+	urlquery := "SELECT name1, name2, name3, name4, name5, name6, name7, name8, name9, name10, name11, name12, name13, name14, name15, name16, name17, name18, name19, name20, name21, name22, name23, name24, name25, name26, name27, name28, name29, name30 FROM traceroutes WHERE url_id=" + ping.urlid
 	err := db.QueryRow(urlquery).Scan(names...)
 	if err != nil {
 		// fmt.Printf("Error: ", err)
@@ -172,9 +277,9 @@ func calculatePath(db *sql.DB, ping Ping) {
 
 	p := 0
 	for _, name := range container {
-		if strings.ContainsRune(name, '.'){
+		if strings.ContainsRune(name, '.') {
 			full_domain_name := strings.Split(name, ".")
-			domain_name := full_domain_name[len(full_domain_name) - 3]+"."+full_domain_name[len(full_domain_name) - 2]
+			domain_name := full_domain_name[len(full_domain_name)-3] + "." + full_domain_name[len(full_domain_name)-2]
 			if p == 0 {
 				path1 = domain_name
 				p++
@@ -187,11 +292,11 @@ func calculatePath(db *sql.DB, ping Ping) {
 			}
 		}
 	}
-	updateQuery := "UPDATE urls SET path1='"+path1+"', path2='"+path2+"', path3='"+path3+"' WHERE id="+strconv.Itoa(ping.urlid)
+	updateQuery := "UPDATE urls SET path1='" + path1 + "', path2='" + path2 + "', path3='" + path3 + "' WHERE id=" + ping.urlid
 	_, err = db.Exec(updateQuery)
 
 	if err != nil {
-		fmt.Printf("Error: ", err)
+		fmt.Printf("Error (calculate_path) : ", err)
 	}
 }
 
@@ -205,12 +310,12 @@ func runServer(db MysqlServer) {
 	var x int = 0
 	var y int = 0
 	for _ = range time.Tick(1 * time.Second) {
-		
+
 		for i, ip := range alexaList {
 			fmt.Println("Doing ", i)
 			// var ping chan Ping = make(chan Ping)
 			var ping Ping
-			ping.urlid = i
+			ping.urlid = strconv.Itoa(i)
 			ping.ipaddr = ip
 			go func() {
 				//var fastPingResult *fastping.Pinger
@@ -231,7 +336,7 @@ func runServer(db MysqlServer) {
 			} else {
 				x++
 			}
-			
+
 		}
 		if y > 9 {
 			y = 0
@@ -242,18 +347,30 @@ func runServer(db MysqlServer) {
 	}
 }
 
+func get_config() Configuration {
+	file, _ := os.Open("conf.json")
+	defer file.Close()
+	fmt.Println(file)
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+  		fmt.Println("error:", err)
+	}
+	fmt.Println(configuration)
+	return configuration	
+}
+
 func main() {
 
-	var l Layout
-
-	l.x_range = 40
-	l.y_range = 25
+	config := get_config()
+	fmt.Println(config)
 
 	var db MysqlServer
-	db.url = "tcp(172.16.1.172:3306)"
-	db.user = "pingtastic"
-	db.password = "pingtastic"
-	db.database = "pingtastic"
+	db.url = "tcp("+config.DBHost+":"+config.DBPort+")"
+	db.user = config.DBUser
+	db.password = config.DBPass
+	db.database = config.DBDatabase
 
 	//argsWithProg := os.Args
 	if os.Args[1] == "getAlexa" {
@@ -263,4 +380,3 @@ func main() {
 	}
 
 }
-
--- swapsort.go
+++ swapsort.go
@@ -1,3 +1,14 @@
+/*
+ * This is a highly inefficient but adaptive sort process to attempt to put urls which are "close" to each other
+ * in order, so patterns emerge in the grid.  Swapsort looks at a URL and the one earlier in the sort, and if the 
+ * current URL has a first, second or third IP hop as being different, it will swap them.  The order in which they
+ * are sorted, will be relatively random, as there is no correlation between lower IP Addresses and anything.
+ *
+ * Though this is currently sorting on the first three hops, it would likely be better to sort on first three
+ * "AS" hops(where AS in this case would be the domain name of the hop)  That would be an easy fix, as we already
+ * have the "AS Path" data kept with the URL.
+ */
+
 package main
 
 import (
@@ -9,30 +20,33 @@ import (
 
 
 func do_swap(pre_urlid int, urlid int, order int, dbcon *sql.DB) {
+    /*
+     * If one URL has a lower IP address than the other URL, swap them.
+     */
     var updateQuery string
 
     updateQuery = "UPDATE urls SET host_order=NULL WHERE id="+strconv.Itoa(pre_urlid)
     _, err := dbcon.Exec(updateQuery)
     if err != nil {
-        fmt.Printf("Error1: ", err)
+        fmt.Printf("Error (do_swap 1) : ", err)
     }
 
     updateQuery = "UPDATE urls SET host_order=NULL WHERE id="+strconv.Itoa(urlid)
     _, err = dbcon.Exec(updateQuery)
     if err != nil {
-        fmt.Printf("Error2: ", err, "\n")
+        fmt.Printf("Error (do_swap 2) : ", err)
     }
 
     updateQuery = "UPDATE urls SET host_order="+strconv.Itoa(order)+" WHERE id="+strconv.Itoa(pre_urlid)
     _, err = dbcon.Exec(updateQuery)
     if err != nil {
-        fmt.Printf("Error3: ", err)
+        fmt.Printf("Error (do_swap 3) : ", err)
     }
 
     updateQuery = "UPDATE urls SET host_order="+strconv.Itoa(order - 1)+" WHERE id="+strconv.Itoa(urlid)
     _, err = dbcon.Exec(updateQuery)
     if err != nil {
-        fmt.Printf("Error4: ", err, "\n")
+        fmt.Printf("Error (do_swap 4) : ", err)
     }
 }
 
@@ -74,13 +88,13 @@ func swapSort(urlid int, dbcon *sql.DB) {
         err = dbcon.QueryRow(unusedQuery).Scan(&next_order)
 
         if err != nil {
-            fmt.Printf("Error111: ", err)
+            fmt.Printf("Error (swapSort 1) : ", err)
         }
 
         updateQuery = "UPDATE urls SET host_order="+strconv.Itoa(next_order)+" WHERE id="+strconv.Itoa(urlid)
         _, err = dbcon.Exec(updateQuery)
         if err != nil {
-            fmt.Printf("Error: ", err)
+            fmt.Printf("Error (swapSort 2) : ", err)
         }
 
         return
@@ -91,19 +105,19 @@ func swapSort(urlid int, dbcon *sql.DB) {
         urlquery = "SELECT id from urls where host_order="+strconv.Itoa(host_order - 1)+" LIMIT 1"
         err = dbcon.QueryRow(urlquery).Scan(&pre_urlid)
         if err != nil {
-            fmt.Printf("Error:222 ", err)
+            fmt.Printf("Error (swapSort 3) : ", err)
         }
 
         urlquery = "SELECT hop1, hop2, hop3 from traceroutes where url_id="+strconv.Itoa(urlid)
         err = dbcon.QueryRow(urlquery).Scan(&pre_hop1, &pre_hop2, &pre_hop3)
         if err != nil {
-            fmt.Printf("Error333: ", err)
+            fmt.Printf("Error (swapSort 4) : ", err)
         }
 
         urlquery = "SELECT hop1, hop2, hop3 from traceroutes where url_id="+strconv.Itoa(pre_urlid)
         err = dbcon.QueryRow(urlquery).Scan(&hop1, &hop2, &hop3)
         if err != nil {
-            fmt.Printf("Error444: ", err)
+            fmt.Printf("Error (swapSort 5) : ", err)
         }
 
         if hop1 < pre_hop1 {
